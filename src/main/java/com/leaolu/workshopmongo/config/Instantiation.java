cd6155483328e1514f80f9d00bc711513cbac417
Lucas Leao <lucasleao@gmail.com>
2024-12-16 17:55:30 -0300

    Post entity assigned with a user

--- src/main/java/com/leaolu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/leaolu/workshopmongo/config/Instantiation.java
@@ -1,20 +1,27 @@
 package com.leaolu.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.leaolu.workshopmongo.domain.Post;
 import com.leaolu.workshopmongo.domain.User;
+import com.leaolu.workshopmongo.repositories.PostRepository;
 import com.leaolu.workshopmongo.repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
 	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -25,7 +32,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "partiu viagem", "vou viajar para sp, abracos", maria);
+		Post post2 = new Post(null, sdf.parse("25/06/2019"), "estou triste", "vou parar de publicar aqui, valeu", bob);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
