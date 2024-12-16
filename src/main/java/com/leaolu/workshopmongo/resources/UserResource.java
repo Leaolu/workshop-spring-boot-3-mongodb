31aaae5f4fee7be276371fba8cd9a114c1a81352
Lucas Leao <lucasleao@gmail.com>
2024-12-16 16:42:24 -0300

    obtaining an user by id

--- src/main/java/com/leaolu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leaolu/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listdto = list.stream().map(x -> new UserDTO(x)).toList();
 		return ResponseEntity.ok().body(listdto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/leaolu/workshopmongo/service/UserService.java
+++ src/main/java/com/leaolu/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.leaolu.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leaolu.workshopmongo.domain.User;
 import com.leaolu.workshopmongo.repositories.UserRepository;
+import com.leaolu.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
