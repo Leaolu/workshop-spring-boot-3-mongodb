77e2031222bb31c6dc8f700ab235dcea68696f11
Lucas Leao <lucasleao@gmail.com>
2024-12-16 16:58:18 -0300

    User insertion with POST

--- src/main/java/com/leaolu/workshopmongo/service/UserService.java
+++ src/main/java/com/leaolu/workshopmongo/service/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.leaolu.workshopmongo.DTO.UserDTO;
 import com.leaolu.workshopmongo.domain.User;
 import com.leaolu.workshopmongo.repositories.UserRepository;
 import com.leaolu.workshopmongo.service.exception.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id){
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		 return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
